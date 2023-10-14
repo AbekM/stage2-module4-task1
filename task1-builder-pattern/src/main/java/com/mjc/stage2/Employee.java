aa2ea304d01a1a35d75815676f00bb11100a9646
AbekM <asilbek.makhmudov@outlook.com>
2023-10-14 19:53:29 +0500

    builder donzo

--- task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
+++ task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
@@ -13,7 +13,13 @@ public class Employee {
     private String email;
     private String carNumber;
 
-    public Employee() {
+    public Employee(EmployeeBuilder employeeBuilder) {
+        this.name = employeeBuilder.name;
+        this.lastName = employeeBuilder.lastName;
+        this.email = employeeBuilder.email;
+        this.position = employeeBuilder.position;
+        this.carNumber = employeeBuilder.carNumber;
+        this.phone = employeeBuilder.phone;
     }
 
     public String getName() {
@@ -69,6 +75,39 @@ public class Employee {
     }
 
     public static class EmployeeBuilder {
-        // Write your code here!
+        private String name;
+        private String lastName;
+        private String position;
+        private String phone;
+        private String email;
+        private String carNumber;
+
+        public EmployeeBuilder() {
+            super();
+        }
+       public EmployeeBuilder setName(String name) {
+           this.name = name;
+           return this;
+       }
+        public EmployeeBuilder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+        public EmployeeBuilder setPosition(String position) {
+            this.position = position;
+            return this;
+        }
+        public EmployeeBuilder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+        public EmployeeBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+        public EmployeeBuilder setCarNumber(String carNumber) {
+            this.carNumber = carNumber;
+            return this;
+        }
     }
 }
